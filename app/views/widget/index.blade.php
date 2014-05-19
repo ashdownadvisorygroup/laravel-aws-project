bead8b7d9e928c21b27c2e1ecb32668899505e30
Peter Mentzer <peter@petermentzerdesign.com>
2014-05-18 17:38:19 -0700

    Adding displays for home/widget

--- app/views/widget/index.blade.php
+++ app/views/widget/index.blade.php
@@ -1,6 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel AWS Project</title>
+	<style>
+	html, body {margin:0;padding:0;font-family:Helvetica,Arial,sans-serif;}
+	.welcome {margin:100px;}
+	</style>
+</head>
+<body>
+	<div class="welcome">
 <table border="0" cellspacing="5" cellpadding="5">
 	<tr><th>Name</th><th>Description</th></tr>
 	@foreach($widgets as $widget) 
 	<tr><td>{{ $widget->name}}</td><td>{{ $widget->description}}</td></tr>
 	@endforeach
 </table>
+</body>
+</html>
