d52c1e62233bad1db8c1f541a18146a4c6a9f18f
Peter Mentzer <peter@petermentzerdesign.com>
2014-05-19 13:08:49 -0700

    more debug

--- app/views/hello.php
+++ app/views/hello.php
@@ -15,8 +15,10 @@
 		<p>This is a sample deployment of the Laravel PHP Framework onto AWS Elastic Beanstalk. It includes an RDS DB, elastic environment detection, and a sample model for <a href="/widget">widgets</a>.</p>
 		<?php $environment = App::environment(); ?>
 		<?php $detectedHostName = gethostname(); ?>
+		<?php $debugStatus = $_ENV['MYSQL_HOST']; ?>
 		<p>Env: <?php echo $environment; ?></p>
 		<p>Host: <?php echo $detectedHostName; ?></p>
+		<p>Debug Status: <?php echo $debugStatus; ?></p>
 		<p></p>
 	</div>
 </body>
